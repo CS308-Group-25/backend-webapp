acd10fb5c8155a909fedf1f9b8d380f3a43f2fb5
Mehmet Emir Karaaslan <memirkaraaslan@gmail.com>
2026-03-26 18:54:13 +0300

    SCRUM-100 T-022 feat(auth): add POST /api/v1/auth/register endpoint

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from modules.auth.router import router as auth_router
+
 load_dotenv()
 
 app = FastAPI(title="SUpplements Store")
-
+app.include_router(auth_router)
 
 @app.get("/")
 def health_check():
