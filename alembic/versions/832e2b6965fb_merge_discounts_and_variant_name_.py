5628c9b7cb80015e9c09a71012cb9a98fb495144
Mehmet Emir Karaaslan <memirkaraaslan@gmail.com>
2026-05-06 00:15:28 +0300

    ruff lint fix B-011 SCRUM-269

--- alembic/versions/832e2b6965fb_merge_discounts_and_variant_name_.py
+++ alembic/versions/832e2b6965fb_merge_discounts_and_variant_name_.py
@@ -7,10 +7,6 @@ Create Date: 2026-05-06 00:08:16.561711
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = '832e2b6965fb'
 down_revision: Union[str, None] = ('a3f9b2c1d8e5', 'e77703bdd591')
