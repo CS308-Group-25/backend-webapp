3e0edfc8c863b97d70a7f738985bf6aad052eb21
Mehmet Emir Karaaslan <memirkaraaslan@gmail.com>
2026-05-06 02:19:26 +0300

    ruff lint fix

--- alembic/versions/d47993e9edec_merge_heads.py
+++ alembic/versions/d47993e9edec_merge_heads.py
@@ -7,10 +7,6 @@ Create Date: 2026-05-06 02:17:15.439868
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
 revision: str = 'd47993e9edec'
 down_revision: Union[str, None] = ('832e2b6965fb', 'f2a7d9c4e1b0')
