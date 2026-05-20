e71034ab6e9aebe5502c77d2a095e83e6093766f
TulayEce <tecey03@gmail.com>
2026-05-20 16:03:11 +0300

    SCRUM-233 SCRUM-234 T-403 T-404 fix(reports): save service file with profit and chart data logic

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
-import core.models      # noqa: E402, F401
-from modules.auth.router import router as auth_router   # noqa: E402
-from modules.cart.router import router as cart_router   # noqa: E402
-from modules.categories.router import (                 # noqa: E402
+import core.models  # noqa: E402, F401
+from modules.auth.router import router as auth_router  # noqa: E402
+from modules.cart.router import router as cart_router  # noqa: E402
+from modules.categories.router import (  # noqa: E402
     admin_router as categories_admin_router,
 )
-from modules.categories.router import router as categories_router           # noqa: E402
-from modules.discounts.router import router as discounts_router             # noqa: E402
-from modules.invoices.router import admin_router as invoices_admin_router   # noqa: E402
-from modules.invoices.router import router as invoices_router               # noqa: E402
-from modules.orders.router import admin_router as orders_admin_router       # noqa: E402
-from modules.orders.router import router as orders_router                   # noqa: E402
-from modules.products.router import admin_router as products_admin_router   # noqa: E402
-from modules.products.router import router as products_router               # noqa: E402
-from modules.refunds.router import admin_router as refunds_admin_router     # noqa: E402
-from modules.refunds.router import router as refunds_router                 # noqa: E402
-from modules.reviews.router import admin_router as reviews_admin_router     # noqa: E402
-from modules.reviews.router import router as reviews_router                 # noqa: E402
-from modules.wishlist.router import router as wishlist_router               # noqa: E402
-from modules.reports.router import router as reports_router                 # noqa: E402
-
+from modules.categories.router import router as categories_router  # noqa: E402
+from modules.discounts.router import router as discounts_router  # noqa: E402
+from modules.invoices.router import admin_router as invoices_admin_router  # noqa: E402
+from modules.invoices.router import router as invoices_router  # noqa: E402
+from modules.orders.router import admin_router as orders_admin_router  # noqa: E402
+from modules.orders.router import router as orders_router  # noqa: E402
+from modules.products.router import admin_router as products_admin_router  # noqa: E402
+from modules.products.router import router as products_router  # noqa: E402
+from modules.refunds.router import admin_router as refunds_admin_router  # noqa: E402
+from modules.refunds.router import router as refunds_router  # noqa: E402
+from modules.reports.router import router as reports_router  # noqa: E402
+from modules.reviews.router import admin_router as reviews_admin_router  # noqa: E402
+from modules.reviews.router import router as reviews_router  # noqa: E402
+from modules.wishlist.router import router as wishlist_router  # noqa: E402
 
 app = FastAPI(title="SUpplements Store")
 
